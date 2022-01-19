d74113fe42720c180c5267106fd56cb01686cb17
Jatin1o1 <r.jatin445@gmail.com>
2022-01-20 00:08:32 +0530

    update in stream all working

---
+++ sktech/stream_PC_2_display/jatin.py
@@ -0,0 +1,28 @@
+import cv2
+import websocket # pip3 install websocket-client
+
+# Area of target display (here 240x240 ST7789)
+
+
+ws = websocket.WebSocket()
+ws.connect("ws://192.168.4.1:8888")
+
+
+#cam = cv2.VideoCapture("/home/jatin/Videos/kungfu.mkv")  # place video file
+cam = cv2.VideoCapture(0)  # webcam
+
+
+def stream_out():
+
+    print("Streaming started...")
+
+  
+    while True:
+        ret, img = cam.read()
+        resize = cv2.resize(img, (240, 150)) 
+        is_success, im_buf_arr = cv2.imencode(".jpg", resize)
+        byte_im = im_buf_arr.tobytes()
+        ws.send(byte_im)
+
+
+stream_out()
