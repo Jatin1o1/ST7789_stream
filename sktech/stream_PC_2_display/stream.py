f9be0f3e40e94cbd061bee096b881959ee8d7c5b
Jatin Goyal <40102297+Jatin1o1@users.noreply.github.com>
2022-02-07 13:26:49 +0530

    Added try catch to catch run time error, Occurring in current setup

--- sktech/stream_PC_2_display/stream.py
+++ sktech/stream_PC_2_display/stream.py
@@ -1,5 +1,7 @@
 import cv2
 import websocket # pip3 install websocket-client
+import time
+import traceback
 
 # Area of target display (here 240x240 ST7789)
 #connect to ssid " Glasses " with pass "12345678"
@@ -10,20 +12,24 @@ ws.connect("ws://192.168.4.1:8888")
 
 #cam = cv2.VideoCapture("/home/jatin/Videos/kungfu.mkv")  # place video file
 cam = cv2.VideoCapture(0)  # webcam
-
+start_time=0
 
 def stream_out():
 
     print("Streaming started...")
+    start_time=time.time()
+    
+    try:
+        while True:
+            ret, img = cam.read()
+            resize = cv2.resize(img, (250, 180)) 
+            is_success, im_buf_arr = cv2.imencode(".jpg", resize)
+            byte_im = im_buf_arr.tobytes()
+            ws.send(byte_im)
+    except Exception as e:
+        print("error occured at time: ",( time.time()- start_time))
+        print(e)
+        traceback.print_exc()
 
-  
-    while True:
-        ret, img = cam.read()
-        resize = cv2.resize(img, (250, 180)) 
-        is_success, im_buf_arr = cv2.imencode(".jpg", resize)
-        byte_im = im_buf_arr.tobytes()
-        ws.send(byte_im)
-
-
+        
 stream_out()
-
